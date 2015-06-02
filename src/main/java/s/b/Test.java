84730521152fadcb35a5df4c19a2c43ec0468abb
gujiahong <13435748540@139.com>
2015-06-02 23:10:07 +0800

    Update Test.java

--- src/main/java/s/b/Test.java
+++ src/main/java/s/b/Test.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//test controller.
 public class Test extends HttpServlet {
 
 	private static final long serialVersionUID = -6704704709301346456L;
